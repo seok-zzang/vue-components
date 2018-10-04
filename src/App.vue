349ebcce40c180ea41c488c8aa22758796c86a0f
falcon <falcon@falcon.local>
2018-10-04 17:31:08 +0900

    Tag Component 추가

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,42 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <b-form-group
+      label="태그"
+      label-for="basicStatic"
+      :label-cols="2"
+      :horizontal="true">
+      <b-form-input-tag class="form-control" 
+        v-model="tags"
+        :limit="10" 
+        :placeholder="'최대 10개까지 등록 가능합니다.'"
+        :separators="','"></b-form-input-tag>
+    </b-form-group>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import bFormInputTag from './components/bFormInputTag'
 
 export default {
   name: 'app',
+  data() {
+    return {
+      tags: 'tag1,tag2,tag3,tag4,tag5'
+    }
+  },
   components: {
-    HelloWorld
+    bFormInputTag
   }
 }
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
